4b286710b3587ab7cd70a9dcc7acf8327b912e86
Carl Olsen <unstoppablecarlolsen@gmail.com>
2016-01-28 12:20:14 -0500

    exception params not optional

--- src/Exceptions/PageRouteBinderNotFoundException.php
+++ src/Exceptions/PageRouteBinderNotFoundException.php
@@ -8,12 +8,12 @@ use Exception;
 
 class PageRouteBinderNotFoundException extends Exception {
 
-    public function __construct($PageRouteBinderClass, $pageType = null, $pageId = null, $pagePath = null) {
+    public function __construct($PageRouteBinderClass, $pageType, $pageId, $pagePath) {
 
         $msg = 'Page Route Binder Class: "%s" Not Found.
 When trying to bind page with: page_type = "%s", id = "%s", path = "%s"';
         $msg = sprintf($msg, $PageRouteBinderClass, $pageType, $pageId, $pagePath);
-        
+
         parent::__construct($msg);
     }
 }
